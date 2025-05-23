6594e169c80365fcb7a52928894b503e72d1848e
Ganeth <83027242+Seetsi1997@users.noreply.github.com>
2025-05-23 10:19:06 +0200

    added abstract class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,33 +1,161 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
 
+// Abstract
+abstract  class StudentMain{
+    abstract void  addNewStudent(Student newStudents) throws SQLException;
+    abstract List<Student> displayAllStudent() throws  SQLException;
+    abstract void updateStudentDetails(Student updateDetails) throws  SQLException;
+    abstract double calculateAllMarksAvg (List<Student> calculateAvg) throws  SQLException;
+    abstract List<Student> searchStudent(Student search) throws  SQLException;
+    abstract void deleteStudent(int studentId) throws SQLException;
+    abstract boolean studentExists(Student exists) throws  SQLException;
+}
 
 class Student {
-    String name;
-    int score;
-    int grade;
+    //Variables
+    private static int idCounter = 1;
+    private int id;
+    private int score;
+    private int grade;
+    private String name;
 
-    Student(String name, int score) {
-        this.name = name;
-        this.score = score;
-        this.grade = 10;
-
+    // Constructor
+    public Student(String sName, int sMarks) {
+        setId(idCounter++);
+        setName(sName);
+        setScore(sMarks);
+        setGrade(10);
     }
 
+    //Setters
+    public void setId(int sId) {
+        if (sId <= 0) {
+            System.out.println("Invalid ID, must be positive");
+            return;
+        }
+        this.id = sId;
+    }
+
+    public void setName(String sName) {
+        if (sName == null || sName.trim().isEmpty()) {
+            System.out.println("Name cannot be empty");
+            return;
+        }
+
+        if (!sName.matches("[a-zA-Z ]+")) {
+            System.out.println("Only letters and spaces are allowed");
+            return;
+        }
+
+        if (sName.length() > 50) {
+            System.out.println("Maximum 50 characters are allowed");
+            return;
+        }
+
+        this.name = sName;
+    }
+
+    public void setScore(int sMarks) {
+        if (sMarks < 0 || sMarks > 100) {
+            System.out.println("Score must be between 0-100.");
+            return;
+        }
+        this.score = sMarks;
+    }
+
+    public void setGrade(int sGrade) {
+        if (sGrade != 10) {
+            System.out.println("Only grade 10 students allowed");
+            return;
+        }
+        this.grade = sGrade;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student Details {" +
+                " Student ID: " + getId() +
+                ", Name: " + getName() +
+                ", Marks: " + getScore() +
+                ", Grade: " + getGrade() +
+                " }";
+    }
 }
+
+
+class StudentMainExtend extends StudentMain{
+    @Override
+    public  void  addNewStudent(Student newStudents) throws  SQLException{}
+    @Override
+    public  List<Student> displayAllStudent() throws SQLException {
+
+
+        var students = new ArrayList<Student>();
+        String sql  = "SELECT * FROM stud ORDER BY id";
+
+        try (Connection conn =  Database.getConnection();
+             Statement stmt = conn.createStatement()) {
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                students.add(new Student(
+                        rs.getString("name"),
+                        rs.getInt("marks")
+                ));
+            }
+        } catch (SQLException e){
+            System.out.println("Student details not available " + e.getMessage());
+        }
+        return students;
+    }
+    @Override
+    public void updateStudentDetails(Student updateDetails) throws SQLException{}
+    @Override
+    public double calculateAllMarksAvg(List<Student> calculateAvg) throws  SQLException{
+        return  0;
+    }
+    @Override
+    public List<Student> searchStudent(Student search) throws  SQLException{
+        return  new ArrayList<>();
+    }
+    @Override
+    public void deleteStudent(int studentId) throws SQLException{}
+    @Override
+    public boolean studentExists(Student student)throws SQLException{
+        return  false;
+    }
+}
+
+
 class Database {
 
     private static final String url = "jdbc:postgresql://localhost:5432/postgres";
     private static final String user = "postgres";
-    private static final String password = "root";
+    private static final String password = "Letsdoit!";
 
     public static void initDB() {
         try (Connection conn = DriverManager.getConnection(url, user, password);
@@ -49,7 +177,13 @@ class Database {
             System.out.println("Database initialization failed: " + e.getMessage());
         }
     }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
 }
+
+
 public class Main {
     public static void main(String[] args) {
         Database.initDB();
@@ -96,7 +230,7 @@ public class Main {
         System.out.println("\n--- Student List ---");
         for (int i = 0; i < students.size(); i++) {
             Student s = students.get(i);
-            System.out.println((i + 1) + ". Name: " + s.name + ", Score: " + s.score + ", Grade: " + s.grade);
+            System.out.println((i + 1) + ". Name: " + s.getName() + ", Score: " + s.getScore()+ ", Grade: " + s.getGrade());
         }
     }
 
@@ -159,7 +293,7 @@ public class Main {
                         System.out.print("Enter new name: ");
                         newName = input.nextLine();
 
-                        students.get(index).name = newName;
+                        students.get(index).setName(newName);
 
                         System.out.println("Student updated " + newName);
                     }else {
@@ -177,7 +311,7 @@ public class Main {
                         System.out.print("Enter new score: ");
                         int score = Integer.parseInt(input.nextLine());
 
-                        students.get(index).score = score;
+                        students.get(index).setScore(score);
 
                         System.out.println("Student updated score" );
                     } else {
@@ -218,7 +352,7 @@ public class Main {
 
             if (index >= 0 && index < students.size()) {
                 Student toDelete = students.get(index);
-                System.out.print("Are you sure you want to delete " + toDelete.name + "? (y/n): ");
+                System.out.print("Are you sure you want to delete " + toDelete.getName() + "? (y/n): ");
                 String confirm = input.nextLine().trim().toLowerCase();
 
                 if (confirm.equals("y")) {
@@ -254,7 +388,7 @@ public class Main {
 
         int total = 0;
         for (Student s : students) {
-            total += s.score;
+            total += s.getScore();
         }
 
         double average = (double) total / students.size();
@@ -270,8 +404,8 @@ public class Main {
         boolean found = false;
 
         for (Student s : students) {
-            if (s.name.toLowerCase().equals(nameToSearch)) {
-                System.out.println("Found: Name: " + s.name + ", Score: " + s.score + ", Grade: " + s.grade);
+            if (s.getName().toLowerCase().equals(nameToSearch)) {
+                System.out.println("Found: Name: " + s.getName() + ", Score: " + s.getScore() + ", Grade: " + s.getGrade());
                 found = true;
             }
         }
