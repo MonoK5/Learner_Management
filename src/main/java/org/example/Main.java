49fb5babc5baeb1d106bf9bdc4c49ca855a1ccdf
khosatho <monokhosatho@gmail.com>
2025-05-27 16:13:20 +0200

    edited update function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -110,7 +110,7 @@ studentMainExtend.displayAllStudent();
 
 
         while (studentToUpdate == null) {
-            System.out.print("Enter student ID to update: ");
+            System.out.print("Enter student ID:");
             String idInput = input.nextLine();
 
             try {
